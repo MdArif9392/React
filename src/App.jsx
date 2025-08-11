52ed5a6f4c8bd8cb9bfd54fd513263ab12bf0e6f
Your Name <you@example.com>
2025-08-11 16:36:33 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Message from "./Message/Message";
+let App = () => {
+  return (
+    <div>
+      <h3>App Component</h3>
+      <hr />
+      <Message />
+    </div>
+  );
+};
+export default App;
