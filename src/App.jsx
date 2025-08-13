9c619ad36c3c76d6d9ce71ab5777acb661889b40
Your Name <you@example.com>
2025-08-13 15:59:26 +0530

    all files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Movies from "./Movies/Movies-1";
 function App() {
   return (
     <div>
       <h2>App Component</h2>
+      <hr />
+      <Movies />
     </div>
   );
 }
