ac2c410937db9e31e920b56d4ce18ace7e6ffb7c
Your Name <you@example.com>
2025-08-22 10:56:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Message from "./Message/Message";
+const App = () => {
+  return (
+    <div>
+      <h4>App component</h4>
+      <hr />
+      <Message />
+    </div>
+  );
+};
+
+export default App;
